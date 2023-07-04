684fa9e653c075d47b210fd4af8fb0a8558dcabb
叶瑶隆 <yaolong.ye@dbappsecurity.com.cn>
2023-07-04 08:56:49 +0800

    chore: 暂存

--- 22_TexturedQuad.js/index.js
+++ 22_TexturedQuad.js/index.js
@@ -34,6 +34,8 @@ function main() {
 
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.log("Failed to initialize shaders");
+
+        return;
     }
 
     // 设置顶点位置
@@ -65,7 +67,7 @@ function initVertexBuffers(gl) {
         -0.5, 0.5, 0.0, 1.0,    // 顶点坐标x, 顶点坐标y, 纹理坐标x, 纹理坐标y
         -0.5, -0.5, 0.0, 0.0,
         0.5, 0.5, 1.0, 1.0,
-        0.5, -0.5, 1.0, 1.0
+        0.5, -0.5, 1.0, 0.0
     ]);
 
     let n = 4;  // 顶点数目
@@ -75,7 +77,7 @@ function initVertexBuffers(gl) {
 
     if (!vertextTexCoordBuffer) {
         console.log('Failed to create the buffer object');
-        return false;
+        return -1;
     }
 
     // 将顶点坐标和纹理坐标写入缓冲区对象
@@ -91,7 +93,11 @@ function initVertexBuffers(gl) {
 
     // 将纹理坐标分配给 a_TexCoord 并开启它
     let a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+    if (a_TexCoord < 0) {
+        console.log("Failed to get the storage location of a_TexCoord");
 
+        return -1;
+    }
     gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
     gl.enableVertexAttribArray(a_TexCoord); // 开启 a_TexCoord
 
@@ -100,12 +106,20 @@ function initVertexBuffers(gl) {
 
 function initTextures(gl, n) {
     let texture = gl.createTexture();   // 创建纹理对象
-
+    if (!texture) {
+        console.log("Failed to create the texture object");
+    }
     // 获取 u_Sampler 的存储位置
     let u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
 
     // 创建一个 image 对象
     let image = new Image();
+    if (!image) {
+        console.log("Failed to create the image object");
+
+        return false;
+    }
+
 
     // 注册图像加载事件的响应函数
     image.onload = function () {
@@ -128,10 +142,12 @@ function loadTexture(gl, n, texture, u_Sampler, image) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
     // 配置纹理图像
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
     // 将 0 号纹理传递给着色器
     gl.uniform1i(u_Sampler, 0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // 绘制矩形
 }
